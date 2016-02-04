e1b043176fa885ec22230c547a668ff736f48088
Roberto Vasquez Angel <roberto@vasquez-angel.de>
2016-02-04 16:41:52 +0100

    Installer now installs routes. Renamed initializer template. Better translations.

--- lib/generators/ecm/contact/install/install_generator.rb
+++ lib/generators/ecm/contact/install/install_generator.rb
@@ -6,9 +6,15 @@ module Ecm
 
         source_root File.expand_path('../templates', __FILE__) 
         
-        def generate_intializer
-          copy_file "ecm_contact.rb", "config/initializers/ecm_contact.rb"
+        def generate_initializer
+          copy_file "initializer.rb", "config/initializers/ecm_contact.rb"
         end   
+
+        def generate_routes
+          inject_into_file 'config/routes.rb', before: "\nend" do
+            File.read(File.join(File.expand_path('../templates', __FILE__), 'routes.source'))
+          end
+        end
       end
     end
   end
