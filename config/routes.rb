9386603e297c5a1e618be23af2a082e0354ea783
Roberto Vasquez Angel <roberto@vasquez-angel.de>
2017-06-11 00:16:50 +0200

    Move to .ruby-* files. Refactor locales to one file per language. Cleanup.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Ecm::Contact::Engine.routes.draw do
   localized do
-    scope :ecm_contact do
+    scope :ecm_contact_engine do
       resources :contact_requests, only: [:create, :index]
+
       root to: 'contact_requests#index'
     end
   end
