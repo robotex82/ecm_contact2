fdc883424fdbed4ab124988d6242367faa00602f
Roberto Vasquez Angel <roberto@vasquez-angel.de>
2018-03-24 14:54:19 +0100

    #cleanup

--- app/controllers/ecm/contact/contact_requests_controller.rb
+++ app/controllers/ecm/contact/contact_requests_controller.rb
@@ -52,22 +52,6 @@ module Ecm
       def after_create_url
         Ecm::Contact::Configuration.after_create_url.call(self)
       end
-
-      # def index
-      #   @title = I18n.t('ecm.contact.request.index.title')
-      #   @contact_request = ContactRequest.new
-      # end
-
-      # def create
-      #   @title = I18n.t('ecm.contact.request.index.title')
-      #   @contact_request = ContactRequest.new(permitted_params)
-
-      #   if @contact_request.deliver
-      #     redirect_to(after_create_url, notice: -> { I18n.t('ecm.contact.form.messages.delivered') }.call)
-      #   else
-      #     render :index
-      #   end
-      # end
     end
   end
 end
